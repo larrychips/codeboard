c552594aa9291ab657ed6e6f79e8884297593034
Larry Chips <larry@larrychips.net>
2025-07-26 00:23:59 +0100

    Set fitsSystemWindows everywhere.

--- app/src/main/java/com/gazlaws/codeboard/layout/ui/KeyboardUiFactory.java
+++ app/src/main/java/com/gazlaws/codeboard/layout/ui/KeyboardUiFactory.java
@@ -31,6 +31,7 @@ public class KeyboardUiFactory {
             View view = createKeyView(context, key, uiTheme);
             layout.addView(view,params);
         }
+        layout.setFitsSystemWindows(true);
         return layout;
     }
 
